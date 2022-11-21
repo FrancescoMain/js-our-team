0c7aa8dc43381985a82962a5ba4514d7982d9e11
Francesco <cesaranofrancescomain@gmail.com>
2022-11-21 18:35:32 +0100

    Elementi in html riscritti utilizzando js

--- script/script.js
+++ script/script.js
@@ -45,40 +45,49 @@ const listaMembri = [
   // 1.1: creiamo un ciclo che ripercorre l'array 
   for (let index = 0; index < listaMembri.length; index++) {
     // 2.1:dichiaro le varibili
-    let col = eleId("obj"+(index+1));
+    let rigaCont = eleId("riga");
     let membroIesimo = listaMembri[index];
     // 2.2 aggiungo le classi al container 
-    addClass(col,"ms_card");
-    
+    let col = creaEle("div")
+    addClass(col,"col")
+    addClass(col,"ms_card")
+    rigaCont.append(col)
+    let containerFoto = creaEle("div");
+    let containerTesto = creaEle("div");
+    addClass(containerTesto, "ms_testo")
+
   
     // 1.2: creaiamo un ciclo che ripercorre le caratteristiche degli oggetti 
     for (let info in membroIesimo) {
 
       // 2.3 creo gli elementi
       let contenuto = membroIesimo[info];
-      let container = creaEle("div");
+
+
+
       // 2.4 aggiungo le classi al container 
-      addClass(container,"ms_card_obj");
 
       // BONUS 1:
       // Trasformare la stringa foto in una immagine effettiva
 
       // B1.1: se sto analizzando le foto le sostituisco con un imagine effettiva
       if (info == "foto") {
+        // creo il container per le foto 
         
         let foto = creaEle("img");
         foto.src = "img/" + membroIesimo[info];
         membroIesimo[info] = foto;
-        console.log(membroIesimo);
         // B1.2 metto le foto in un container diverso per averle sopra al resto 
-        let cont = eleId("foto"+(index+1));
-        cont.append(foto)
+        containerFoto.append(foto);
+
         
       } else {
 
       // 2.5 appendo il contenuto restante  al container e alla colonna 
-      container.append(contenuto);
-      col.append(container) ;
+      let containerFiglio =creaEle("div")
+      containerFiglio.append(contenuto)
+      containerTesto.append(containerFiglio);
+
 
       }
 
@@ -86,6 +95,10 @@ const listaMembri = [
 
       
     }
+
+    col.append(containerFoto);
+    addClass(containerFoto,"foto")
+    col.append(containerTesto);
   }
 
 // MILESTONE 2:
