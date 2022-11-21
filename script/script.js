ea87833b26a5d90b3d07cfe9524e0f1fbd40efd4
Francesco <cesaranofrancescomain@gmail.com>
2022-11-21 14:10:20 +0100

    stampo su console le info degli ogetti

--- script/script.js
+++ script/script.js
@@ -8,39 +8,53 @@ const listaMembri = [
 
   {
     "nome" : "Wayne Barnett",
-    "cognome" : "Founder & CEO",
+    "ruolo" : "Founder & CEO",
     "foto" : "wayne-barnett-founder-ceo.jpg"
   },
   {
     "nome" : "Angela Caroll",
-    "cognome" : "Chief Editor",
+    "ruolo" : "Chief Editor",
     "foto" : "angela-caroll-chief-editor.jpg"
   },
   {
     "nome" : "Walter Gordon",
-    "cognome" : "Office Manager",
+    "ruolo" : "Office Manager",
     "foto" : "walter-gordon-office-manager.jpg"
   },
   {
     "nome" : "Angela Lopez",
-    "cognome" : "Social Media Manager",
+    "ruolo" : "Social Media Manager",
     "foto" : "angela-lopez-social-media-manager.jpg"
   },
   {
     "nome" : "Scott Estrada",
-    "cognome" : "Developer",
+    "ruolo" : "Developer",
     "foto" : "scott-estrada-developer.jpg"
   },
   {
     "nome" : "Barbara Ramos",
-    "cognome" : "Graphic Designer",
+    "ruolo" : "Graphic Designer",
     "foto" : "barbara-ramos-graphic-designer.jpg"
   }
 ];
+
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// 1.1: creiamo un ciclo che ripercorre l'array 
+for (let index = 0; index < listaMembri.length; index++) {
+  let membroIesimo = listaMembri[index];
+  console.log(membroIesimo);
+  // 1.2: creaiamo un ciclo che ripercorre le caratteristiche degli oggetti 
+  for (let info in membroIesimo) {
+      console.log(info, membroIesimo[info]);
+    
+  }
+}
+
 // MILESTONE 2:
 // Stampare le stesse informazioni su DOM sottoforma di stringhe
+
 // BONUS 1:
 // Trasformare la stringa foto in una immagine effettiva
 // BONUS 2:
